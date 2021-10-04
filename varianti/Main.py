716322d3151c3fdeb950094e810a688abca105f3
singizin <igsing96@gmail.com>
2021-10-04 23:20:49 +0700

    xcle

--- varianti/Main.py
+++ varianti/Main.py
@@ -2,30 +2,34 @@ import random
 
 import pandas as pd
 import sources as s
-import os
+
 
 shablon = ['', '', '', '', '', '']
 
 folder = "/varianti/"
-table_headers = ['пол', 'имя', 'рост', 'вес', 'город', 'балл']
+table_headers = ['пол', 'имя', 'рост', 'вес', 'город', 'балл 2019', 'балл 2020', 'балл 2021']
 
 
 def generate():
     sex = s.sex[random.randint(0, 1)]
     if sex == s.sex[0]:
-        name = s.names_man[random.randint(0, len(s.names_man)-1)]
-    if sex == s.sex[1]:
-        name = s.names_woman[random.randint(0, len(s.names_woman)-1)]
-    city = s.city[random.randint(0, len(s.city)-1)]
+        name = s.names_man[random.randint(0, len(s.names_man) - 1)]
+    else:
+        name = s.names_woman[random.randint(0, len(s.names_woman) - 1)]
+
+    city = s.city[random.randint(0, len(s.city) - 1)]
     weight = random.randint(50, 80)
-    height = random.randint(50, 80)
+    height = random.randint(150, 180)
     point = random.randint(140, 300)
-    print(sex, name, weight, height, city, point)
+    point2 = random.randint(140, 300)
+    point3 = random.randint(140, 300)
+    return [sex, name, weight, height, city, point, point2, point3]
 
 
-for i in range(10):
-    generate()
-# spisok = []
-# df1 = pd.DataFrame(spisok,
-#                    columns=table_headers)
-# df1.to_excel(folder + "output_ee.xlsx")
+for j in range(30):
+    all_people = []
+    for i in range(random.randint(100, 120)):
+        all_people.append(generate())
+    df = pd.DataFrame(all_people, columns=table_headers)
+    print(df.to_string())
+    df.to_excel(f'generated/{j}.xlsx')
